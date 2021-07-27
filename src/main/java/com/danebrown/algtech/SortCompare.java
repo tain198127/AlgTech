d1dc91c6f25c70796d8cbaad016a9af41eb81b72
dane brown <tain198127@163.com>
2021-07-27 23:25:50 +0800

    调整打印问题

--- src/main/java/com/danebrown/algtech/SortCompare.java
+++ src/main/java/com/danebrown/algtech/SortCompare.java
@@ -3,15 +3,11 @@ package com.danebrown.algtech;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.time.StopWatch;
-import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Setup;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 
@@ -27,66 +23,91 @@ public class SortCompare {
     public int size = 50000;
     public int[] array = new int[size];
 
+    public static void main(String[] args) {
+        SortCompare sortCompare = new SortCompare();
+        sortCompare.setupData(2);
+        log.debug("原始数据:{}", sortCompare.array);
+        boolean compareResult = sortCompare.compareWrap(
+                () -> sortCompare.insertSort(),
+                "insertSort",
+                () -> sortCompare.standardSort(),
+                false);
+
+
+    }
+
     /**
      * 装在预备数据
+     *
      * @param mode 0：顺序；1：逆序；2：随机；
      */
     @Setup
-    public void setupData(int mode){
+    public void setupData(int mode) {
         for (int i = 0; i < array.length; i++) {
             array[i] = ThreadLocalRandom.current().nextInt();
         }
         Arrays.sort(array);
-        if(mode == 1){
+        if (mode == 1) {
             ArrayUtils.reverse(array);
         }
-        if(mode == 2){
-           ArrayUtils.shuffle(array);
+        if (mode == 2) {
+            ArrayUtils.shuffle(array);
         }
 
     }
+
     /**
      * 对数器
-     * @param forTest 用于测试的数据
+     *
+     * @param forTest  用于测试的数据
      * @param standard 标准结果
      * @return 是否一致
      */
-    public boolean compare(int[] forTest, int[] standard){
-        if(forTest == null || standard == null){
+    public boolean compare(int[] forTest, int[] standard) {
+        if (forTest == null || standard == null) {
+            log.error("测试数组为空");
             return false;
         }
-        if(forTest.length != standard.length){
+        if (forTest.length != standard.length) {
+            log.error("长度不一致");
             return false;
         }
         boolean result = true;
         int length = forTest.length;
-        for(int i = 0; i < length && result;i++){
-            result = forTest[i]== standard[i];
+        for (int i = 0; i < length && result; i++) {
+            result = forTest[i] == standard[i];
+            if (!result) {
+                log.error("第{}位数据不一致，原始数据:{};测试数据:{}", i, standard[i], forTest[i]);
+            }
         }
         return result;
     }
+
     /**
      * 标准算法，正确性验证
+     *
      * @return
      */
-    public int[] standardSort(){
+    public int[] standardSort() {
         int[] result = Arrays.stream(array).sorted().toArray();
         return result;
     }
 
     /**
      * 插入排序
+     *
      * @return
      */
-    public int[] insertSort(){
+    public int[] insertSort() {
         int[] cp = ArrayUtils.clone(array);
-        for(int i = 1; i < cp.length;i++){
-            for(int j = i-1; j >= 0 && cp[j] > cp[j+1]; j--){
-                innerSwap(cp,j,j+1);
+        for (int i = 1; i < cp.length; i++) {
+            for (int j = i - 1; j >= 0 && cp[j] > cp[j + 1]; j--) {
+                innerSwap(cp, j, j + 1);
             }
         }
         return cp;
     }
+
     private void innerSwap(int[] cp, int j, int i) {
         cp[i] = cp[i] ^ cp[j];
         cp[j] = cp[i] ^ cp[j];
@@ -95,54 +116,38 @@ public class SortCompare {
 
     /**
      * 封装对比方法
+     *
      * @param forTest
      * @param standard
      * @return
      */
-    public boolean compareWrap(Supplier<int[]> forTest,String testMethodName,
-                               Supplier<int[]> standard,boolean isCompare){
+    public boolean compareWrap(Supplier<int[]> forTest, String testMethodName, Supplier<int[]> standard, boolean isCompare) {
         StopWatch stopWatch = new StopWatch();
 
         stopWatch.start();
-        int[] standardArr = standard.get();
-        stopWatch.stop();
+        int[] test = forTest.get();
 
-        log.info("测试计算耗时:{} 毫秒",stopWatch.getTime(TimeUnit.MILLISECONDS));
+        stopWatch.stop();
+        log.info("测试计算耗时:{} 毫秒", stopWatch.getTime(TimeUnit.MILLISECONDS));
+
+
         boolean result = !isCompare;
-        if(isCompare){
+        if (isCompare) {
             result = true;
             stopWatch.reset();
             stopWatch.start();
-            int[] test = forTest.get();
+            int[] standardArr = standard.get();
             stopWatch.stop();
-            log.info("标准计算耗时:{} 毫秒",stopWatch.getTime(TimeUnit.MILLISECONDS));
-
-
-            result = compare(test,standardArr);
-            log.debug("标准结果:{}",standardArr);
-            log.debug("测试结果:{}",test);
-            if(!result){
-                log.error("{}测试失败",testMethodName);
-            }
-            else{
-                log.info("{}测试成功",testMethodName);
+            log.info("标准计算耗时:{} 毫秒", stopWatch.getTime(TimeUnit.MILLISECONDS));
+            result = compare(test, standardArr);
+            log.debug("标准结果:{}", standardArr);
+            log.debug("测试结果:{}", test);
+            if (!result) {
+                log.error("{}测试失败", testMethodName);
+            } else {
+                log.info("{}测试成功", testMethodName);
             }
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        SortCompare sortCompare = new SortCompare();
-        sortCompare.setupData(2);
-        log.debug("原始数据:{}",sortCompare.array);
-        int[] result = sortCompare.standardSort();
-        log.debug("standardSort:{}",result);
-
-        boolean compareResult =
-                sortCompare.compareWrap(() -> sortCompare.insertSort(),
-                "insertSort",
-                () -> sortCompare.standardSort(),true);
-
-
     }
 }
