0b84dfbca0616cda293692936ab3f7bd1307b5ea
dane brown <tain198127@163.com>
2021-08-09 16:46:32 +0800

    完成非递归归并排序

--- src/main/java/com/danebrown/algtech/MergeSort.java
+++ src/main/java/com/danebrown/algtech/MergeSort.java
@@ -23,14 +23,15 @@ public class MergeSort {
         AlgCompMenu.addComp(new BiggerThanRightTwice());
         AlgCompMenu.run();
     }
+
     @AlgName("递归归并排序[完成]")
-    public static class RecursionMergeSort extends AlgCompImpl<int[],int[]> {
+    public static class RecursionMergeSort extends AlgCompImpl<int[], int[]> {
 
         @Override
         public int[] prepare() {
             int dataSize = ThreadLocalRandom.current().nextInt(2, 500000);
-            int [] data = new int[dataSize];
-            for(int i=0;i < dataSize; i++){
+            int[] data = new int[dataSize];
+            for (int i = 0; i < dataSize; i++) {
                 data[i] = ThreadLocalRandom.current().nextInt();
             }
             return data;
@@ -47,37 +48,40 @@ public class MergeSort {
             mergeSort(data);
             return data;
         }
-        private void mergeSort(int[] data){
-            mergeSort(data,0,data.length-1);//这里容易出错
+
+        private void mergeSort(int[] data) {
+            mergeSort(data, 0, data.length - 1);//这里容易出错
         }
-        private void mergeSort(int[] data,int L,int R){
-            if(data == null || data.length <2||L ==R){
+
+        private void mergeSort(int[] data, int L, int R) {
+            if (data == null || data.length < 2 || L == R) {
                 return;
             }
             /**
              * 这里很容易出错
              */
-            int middle = L + ((R-L)>>1);//L + (R-L)/2 = R/2 + L/2=(R+L)/2
-            mergeSort(data,L,middle);
-            mergeSort(data,middle+1,R);//这里容易出错
-            merge(data,L,middle,R);
+            int middle = L + ((R - L) >> 1);//L + (R-L)/2 = R/2 + L/2=(R+L)/2
+            mergeSort(data, L, middle);
+            mergeSort(data, middle + 1, R);//这里容易出错
+            merge(data, L, middle, R);
         }
-        private void merge(int[]data,int L,int M, int R){
-            int i=0;
-            int[] tmp = new int[R-L+1];
+
+        private void merge(int[] data, int L, int M, int R) {
+            int i = 0;
+            int[] tmp = new int[R - L + 1];
             int l = L;
-            int r = M+1;
-            while (l <= M && r <= R){//这里容易出错
-                tmp[i++] = data[l] <= data[r]?data[l++]:data[r++];//谁大用谁
+            int r = M + 1;
+            while (l <= M && r <= R) {//这里容易出错
+                tmp[i++] = data[l] <= data[r] ? data[l++] : data[r++];//谁大用谁
             }
-            while (l <= M){
+            while (l <= M) {
                 tmp[i++] = data[l++];//
             }
-            while (r <= R){
+            while (r <= R) {
                 tmp[i++] = data[r++];
             }
-            for(i=0;i<tmp.length;i++){
-                data[L+i] = tmp[i];
+            for (i = 0; i < tmp.length; i++) {
+                data[L + i] = tmp[i];
             }
         }
     }
@@ -86,9 +90,10 @@ public class MergeSort {
      * 循环版归并排序 第五章
      */
     @AlgName("循环归并排序")
-    public static class WhileMergeSort extends AlgCompImpl<int[],int[]>{
+    public static class WhileMergeSort extends AlgCompImpl<int[], int[]> {
 
-        RecursionMergeSort recursionMergeSort  = new RecursionMergeSort();
+        RecursionMergeSort recursionMergeSort = new RecursionMergeSort();
+
         @Override
         public int[] prepare() {
             return recursionMergeSort.prepare();
@@ -104,57 +109,59 @@ public class MergeSort {
             whileSort(data);
             return data;
         }
-        private void whileSort(int[] data){
-            if(data == null||data.length<2) {
+
+        private void whileSort(int[] data) {
+            if (data == null || data.length < 2) {
                 return;
             }
             int N = data.length;
-            int mergeSize=1;
-            while (mergeSize < N){
+            int mergeSize = 1;
+            while (mergeSize < N) {
                 int L = 0;//当前左组的第一个位置
-                while(L<N)   //这里很容易忘记
-                {
-                    int M = L+mergeSize -1;  //这里容易错
-                    if(M >= N){   //这里容易漏掉
+                while (L < N) {//这里很容易忘记
+                    int M = L + mergeSize - 1;  //这里容易错
+                    if (M >= N) {   //这里容易漏掉
                         break;
                     }
-                    int R = Math.min(M+mergeSize,N-1);   //这里容易错
-                    merge(data,L,M,R);
-                    L=R+1; //这里容易漏掉
+                    int R = Math.min(M + mergeSize, N - 1);   //这里容易错
+                    merge(data, L, M, R);
+                    L = R + 1; //这里容易漏掉
                 }
-                if(mergeSize > N/2){   //这里容易漏掉
+                if (mergeSize > N / 2) {   //这里容易漏掉
                     break;
                 }
                 mergeSize <<= 1;
 
             }
         }
-        private void merge(int[] data,int l,int m,int r){
-            int p1= l;
-            int p2 = m+1;
-            int[] tmp = new int[r-l+1];
+
+        private void merge(int[] data, int l, int m, int r) {
+            int p1 = l;
+            int p2 = m + 1;
+            int[] tmp = new int[r - l + 1];
             int i = 0;
-            while (p1 <= m && p2 <= r){
-                tmp[i++] = data[p1]<=data[p2]?data[p1++]:data[p2++];
+            while (p1 <= m && p2 <= r) {
+                tmp[i++] = data[p1] <= data[p2] ? data[p1++] : data[p2++];
             }
-            while (p1 <= m){
+            while (p1 <= m) {
                 tmp[i++] = data[p1++];
             }
-            while (p2 <= r){
+            while (p2 <= r) {
                 tmp[i++] = data[p2++];
             }
-            for(i=0;i<tmp.length;i++){
-                data[i+l] = tmp[i];
+            for (i = 0; i < tmp.length; i++) {
+                data[i + l] = tmp[i];
             }
         }
     }
 
     /**
-     *  第五章
+     * 第五章
      */
     @AlgName("小和")
-    public static class LeftMinSum extends AlgCompImpl<Integer,int[]>{
+    public static class LeftMinSum extends AlgCompImpl<Integer, int[]> {
         RecursionMergeSort recursionMergeSort = new RecursionMergeSort();
+
         @Override
         public int[] prepare() {
             return recursionMergeSort.prepare();
@@ -173,10 +180,10 @@ public class MergeSort {
     }
 
     /**
-     *  第五章
+     * 第五章
      */
     @AlgName("逆序对计算，经典考题")
-    public static class ReversSortPair extends AlgCompImpl<Integer,int[]>{
+    public static class ReversSortPair extends AlgCompImpl<Integer, int[]> {
 
         @Override
         public int[] prepare() {
@@ -195,12 +202,12 @@ public class MergeSort {
     }
 
     /**
-     *  第五章
+     * 第五章
      */
     @AlgName("大于右侧数2倍")
-    public static class BiggerThanRightTwice extends AlgCompImpl<Integer,int[]>{
-        private RecursionMergeSort recursionMergeSort =
-                new RecursionMergeSort();
+    public static class BiggerThanRightTwice extends AlgCompImpl<Integer, int[]> {
+        private RecursionMergeSort recursionMergeSort = new RecursionMergeSort();
+
         @Override
         public int[] prepare() {
             return recursionMergeSort.prepare();
@@ -221,16 +228,16 @@ public class MergeSort {
      * 给定一个数组，和最大值、最小值，数组中有多少个子数组累加的和在[low,high]内
      */
     @AlgName("滑动范围子数组")
-    public static class CountOfRangeSum extends AlgCompImpl<Integer,
-            Triple<int[],Integer,Integer>>{
+    public static class CountOfRangeSum extends AlgCompImpl<Integer, Triple<int[], Integer, Integer>> {
 
         RecursionMergeSort recursionMergeSort = new RecursionMergeSort();
+
         @Override
         public Triple<int[], Integer, Integer> prepare() {
-           int[] arr = recursionMergeSort.prepare();
-           int low = ThreadLocalRandom.current().nextInt();
-           int high = ThreadLocalRandom.current().nextInt(low,arr.length+low);
-           return Triple.of(arr,low,high);
+            int[] arr = recursionMergeSort.prepare();
+            int low = ThreadLocalRandom.current().nextInt();
+            int high = ThreadLocalRandom.current().nextInt(low, arr.length + low);
+            return Triple.of(arr, low, high);
         }
 
         @Override
