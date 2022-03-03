6bf5a8eebadcaac73e4fb4cafe9397e802701714
dane brown <tain198127@163.com>
2022-03-03 10:50:37 +0800

    最大快乐值

--- src/main/java/com/danebrown/algtech/TreeStaff.java
+++ src/main/java/com/danebrown/algtech/TreeStaff.java
@@ -60,6 +60,7 @@ public class TreeStaff {
         AlgCompMenu.addComp(new IsFullBT());
         AlgCompMenu.addComp(new FindMaxSBT());
         AlgCompMenu.addComp(new LowestAncestors());
+        AlgCompMenu.addComp(new MaxHappy());
         AlgCompMenu.run();
     }
 
@@ -1442,6 +1443,7 @@ public class TreeStaff {
         }
     }
 
+
     /**
      * 给定一个节点X，以及A,B两个节点，返回他们的高度最低的祖先节点。
      * 注意，这里的节点X，可能不是头节点。所以有一种可能是X节点下面，可能不包含A节点或者B节点
@@ -1574,6 +1576,128 @@ public class TreeStaff {
             private boolean cross;
             private BinTreeNode<Integer, String> crossNode;
 
+        }
+    }
+
+
+    /**
+     * 最大欢乐树节点
+     */
+    public static class Employee {
+        public int happy;
+        public List<Employee> nexts;
+
+        public Employee(int h) {
+            happy = h;
+            nexts = new ArrayList<>();
+        }
+
+    }
+
+    /**
+     * 最大欢乐树
+     * 公司的每个员工都符合 Employee 类的描述。整个公司的人员结构可以看作是一棵标准的、
+     * 没有环的多叉树。树的头节点是公司唯一的老板。除老板之外的每个员工都有唯一的直接上级。
+     * 叶节点是没有任何下属的基层员工(subordinates列表为空)，除基层员工外，每个员工都有一个或多个直接下级。
+     * 这个公司现在要办party，你可以决定哪些员工来，哪些员工不来，规则：
+     * 1.如果某个员工来了，那么这个员工的所有直接下级都不能来
+     * 2.派对的整体快乐值是所有到场员工快乐值的累加
+     * 3.你的目标是让派对的整体快乐值尽量大
+     * 给定一棵多叉树的头节点boss，请返回派对的最大快乐值
+     */
+    @AlgName("MaxHappy")
+    public static class MaxHappy extends AlgCompImpl<Integer, Employee>{
+
+        @Override
+        public Employee prepare() {
+            int maxLevel = 4;
+            int maxNexts = 7;
+            int maxHappy = 100;
+            Employee boss = genarateBoss(maxLevel, maxNexts, maxHappy);
+            return boss;
+        }
+        public static Employee genarateBoss(int maxLevel, int maxNexts, int maxHappy) {
+            if (Math.random() < 0.02) {
+                return null;
+            }
+            Employee boss = new Employee((int) (Math.random() * (maxHappy + 1)));
+            genarateNexts(boss, 1, maxLevel, maxNexts, maxHappy);
+            return boss;
+        }
+        public static void genarateNexts(Employee e, int level, int maxLevel, int maxNexts, int maxHappy) {
+            if (level > maxLevel) {
+                return;
+            }
+            int nextsSize = (int) (Math.random() * (maxNexts + 1));
+            for (int i = 0; i < nextsSize; i++) {
+                Employee next = new Employee((int) (Math.random() * (maxHappy + 1)));
+                e.nexts.add(next);
+                genarateNexts(next, level + 1, maxLevel, maxNexts, maxHappy);
+            }
+        }
+
+        @Override
+        protected Integer standard(Employee boss) {
+            if (boss == null) {
+                return 0;
+            }
+            return process1(boss, false);
+        }
+        // 当前来到的节点叫cur，
+        // up表示cur的上级是否来，
+        // 该函数含义：
+        // 如果up为true，表示在cur上级已经确定来，的情况下，cur整棵树能够提供最大的快乐值是多少？
+        // 如果up为false，表示在cur上级已经确定不来，的情况下，cur整棵树能够提供最大的快乐值是多少？
+        public static int process1(Employee cur, boolean up) {
+            if (up) { // 如果cur的上级来的话，cur没得选，只能不来
+                int ans = 0;
+                for (Employee next : cur.nexts) {
+                    ans += process1(next, false);
+                }
+                return ans;
+            } else { // 如果cur的上级不来的话，cur可以选，可以来也可以不来
+                int p1 = cur.happy;
+                int p2 = 0;
+                for (Employee next : cur.nexts) {
+                    p1 += process1(next, true);
+                    p2 += process1(next, false);
+                }
+                return Math.max(p1, p2);
+            }
+        }
+
+        @Override
+        protected Integer test(Employee boss) {
+            MaxHappyPopInfo info = process2(boss);
+            return Math.max(info.join,info.notJoin);
+        }
+        public MaxHappyPopInfo process2(Employee x){
+            if(x == null){
+                return new MaxHappyPopInfo(0,0);
+            }
+            int join = x.happy;
+            int notJoin = 0;
+            for(Employee e : x.nexts){
+
+                MaxHappyPopInfo childInfo = process2(e);
+                //如果x参加的话，那就只能计算x的孩子们不来的值
+                join = join + childInfo.notJoin;
+                notJoin = notJoin + Math.max(childInfo.join,childInfo.notJoin);
+
+            }
+            return new MaxHappyPopInfo(join,notJoin);
+        }
+        @Data
+        @AllArgsConstructor
+        public class MaxHappyPopInfo{
+            /**
+             * 表示X节点来的最大值
+             */
+            int join;
+            /**
+             * 表示X节点不来的最大值
+             */
+            int notJoin;
         }
     }
 }
