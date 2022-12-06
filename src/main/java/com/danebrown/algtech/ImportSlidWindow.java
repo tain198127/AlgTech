ca63432e8f81d956eed33f1a656ec95e641d8cf6
dane brown <tain198127@163.com>
2022-12-07 02:09:34 +0800

    增加滑动窗口问题

--- src/main/java/com/danebrown/algtech/ImportSlidWindow.java
+++ src/main/java/com/danebrown/algtech/ImportSlidWindow.java
@@ -7,6 +7,8 @@ import com.danebrown.algtech.algcomp.AlgName;
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Stack;
 import java.util.concurrent.ThreadLocalRandom;
@@ -19,6 +21,7 @@ public class ImportSlidWindow {
     public static void main(String[] args) {
         AlgCompMenu.addComp(new SlidWindowMaxArray());
         AlgCompMenu.addComp(new SlidWindowMaxArray2());
+        AlgCompMenu.addComp(new SlidSubWindowMaxArray());
         AlgCompMenu.run();
     }
     public enum SlidWindowMaxArrayEnum{
@@ -169,8 +172,12 @@ public class ImportSlidWindow {
         }
         @Override
         public SlidWindowMaxArray2Param prepare() {
+            return prepare(1000);
+        }
+
+        public SlidWindowMaxArray2Param prepare(int size) {
             SlidWindowMaxArray2Param param = new SlidWindowMaxArray2Param();
-            int arrSize = ThreadLocalRandom.current().nextInt(1,10000);
+            int arrSize = ThreadLocalRandom.current().nextInt(1,size);
             int windw = ThreadLocalRandom.current().nextInt(0,arrSize);
             param.arr = new int[arrSize];
             param.w = windw;
@@ -245,6 +252,127 @@ public class ImportSlidWindow {
                 }
             }
             return res;
+        }
+    }
+
+    /**
+     * 给定一个整型数组arr，和一个整数num
+     * 某个arr中的子数组sub，如果想达标，必须满足：
+     * sub中最大值 – sub中最小值 <= num，
+     * 返回arr中达标子数组的数量
+     */
+    @AlgName("滑动达标子数组")
+    public static class SlidSubWindowMaxArray extends AlgCompImpl<Integer,SlidWindowMaxArray2Param>{
+        public enum SlidSubWindowMaxArrayEnum{
+            AI{
+                @Override
+                public Integer cal(SlidWindowMaxArray2Param data) {
+                    int [] arr = data.arr;
+                    int num = data.w;
+                    int n = arr.length;
+                    int res = 0;
+                    Deque<Integer> q = new ArrayDeque<>();
+
+                    // 遍历数组
+                    for (int i = 0; i < n; i++) {
+                        // 将不符合条件的值从队列中删除
+                        while (!q.isEmpty() && arr[i] - arr[q.peekFirst()] > num) {
+                            q.pollFirst();
+                        }
+                        // 插入新的值
+                        while (!q.isEmpty() && arr[i] <= arr[q.peekLast()]) {
+                            q.pollLast();
+                        }
+                        q.offerLast(i);
+                        // 将窗口的左边界向右移动
+                        if(i-num>0) {
+                            while (arr[q.peekFirst()] - arr[i - num] > num) {
+                                q.pollFirst();
+                            }
+                        }
+
+                        // 统计结果
+                        res += i - q.peekFirst() + 1;
+                    }
+
+                    return res;
+                }
+            };
+            public abstract Integer cal(SlidWindowMaxArray2Param data);
+        }
+        SlidWindowMaxArray2 slidWindowMaxArray2 = new SlidWindowMaxArray2();
+        @Override
+        public SlidWindowMaxArray2Param prepare() {
+            return slidWindowMaxArray2.prepare(2000);
+        }
+
+        @Override
+        protected Integer standard(SlidWindowMaxArray2Param data) {
+//            return SlidSubWindowMaxArrayEnum.AI.cal(data);
+            int[] arr = data.arr;
+            int sum = data.w;
+            if (arr == null || arr.length == 0 || sum < 0) {
+                return 0;
+            }
+            int N = arr.length;
+            int count = 0;
+            for (int L = 0; L < N; L++) {
+                for (int R = L; R < N; R++) {
+                    int max = arr[L];
+                    int min = arr[L];
+                    for (int i = L + 1; i <= R; i++) {
+                        max = Math.max(max, arr[i]);
+                        min = Math.min(min, arr[i]);
+                    }
+                    if (max - min <= sum) {
+                        count++;
+                    }
+                }
+            }
+            return count;
+            
+        }
+
+        @Override
+        protected Integer test(SlidWindowMaxArray2Param data) {
+            int w = data.w;
+            int[] arr = data.arr;
+            int N = arr.length;
+            int count = 0;
+            if (arr == null || arr.length == 0 || w < 0) {
+                return 0;
+            }
+            LinkedList<Integer> maxWindow = new LinkedList<>();
+            LinkedList<Integer> minWindow = new LinkedList<>();
+            int R = 0;
+            for(int L = 0; L < N;L++){
+                
+                    while (R < N){
+
+                        while (!maxWindow.isEmpty() && arr[maxWindow.peekLast()] <= arr[R]){
+                            maxWindow.pollLast();
+                        }
+                        maxWindow.addLast(R);
+                        while (!minWindow.isEmpty() && arr[minWindow.peekLast()] >= arr[R]){
+                            minWindow.pollLast();
+                        }
+                        minWindow.addLast(R);
+                        if(arr[maxWindow.peekFirst()] - arr[minWindow.peekFirst()] > w){
+                            break;
+                        }else{
+                            R++;
+                        }
+                    }
+                    count += R-L;
+                    if(maxWindow.peekFirst() == L){
+                        maxWindow.pollFirst();
+                    }
+                    if(minWindow.peekFirst() == L){
+                        minWindow.pollFirst();
+                    }
+                
+            }
+            return count;
         }
     }
 }
